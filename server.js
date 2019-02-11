6ead2cda23cfcb2f4c640ce4330062ad49b61d64
Ewald Van Rooyen <mailmevanrooyen@gmail.com>
2019-02-11 10:06:43 +0200

    Update server.js
    Updated url processing

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const PORT = 3000;
 const SERVER_START_MESSAGE = "YEAY boi it's lit all up in here.";
 
 const server = http.createServer((request,response)=>{
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('My MAN\n');
+
+	response.setHeader('Content-Type', 'text/plain');
+
+	if(request.url === "/")
+	{
+		response.write("Da BASics\n");		
+	}
+	else {
+		response.write("Sum OTHER stuff\n");			
+	}
+
+  response.statusCode = 200;
+ return response.end('My MAN\n');
 })
 
 server.listen(PORT,()=>{
-	console.log(SERVER_START_MESSAGE);
+	console.log(PORT + " " + SERVER_START_MESSAGE);
 });
 
 
